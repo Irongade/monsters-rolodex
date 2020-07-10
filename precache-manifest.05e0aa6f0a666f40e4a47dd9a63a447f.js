35363a1b358ce4c8f2fa35ad529fa52ef9acb8e2
Irongade <adeoluayangade@yahoo.com>
2020-07-10 23:17:23 +0100

    Updates

---
+++ precache-manifest.05e0aa6f0a666f40e4a47dd9a63a447f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e551dd071107ab02910c17855d8724",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa1e372f91b53ac228fd",
+    "url": "/monsters-rolodex/static/css/main.1b026e5b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1e372f91b53ac228fd",
+    "url": "/monsters-rolodex/static/js/main.7c66bc69.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
